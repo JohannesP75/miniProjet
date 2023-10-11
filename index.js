d326763e51cd632012d28520e1362c3a19eb7f90
Julien P <jpoualeu@yahoo.fr>
2023-10-12 01:12:14 +0200

    intermedary avant envoi sur Github

--- index.js
+++ index.js
@@ -3,44 +3,59 @@ const express = require('express');
 const app = express();
 var mysql = require('mysql');
 var cors = require('cors')
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT_MINIPROJECT=3000;
 
 let listTasks=[
     {
+        id: 0,
         name: "alpha",
         dueDate: "2023-12-25",
         isDone: false
     },
     {
+        id: 1,
         name: "beta",
         dueDate: "2022-12-19",
         isDone: false
     },
     {
+        id: 2,
         name: "gamma",
         dueDate: "2023-07-14",
         isDone: true
     },
     {
+        id: 3,
         name: "delta",
         dueDate: "2020-01-04",
         isDone: false
     },
     {
+        id: 4,
         name: "epsilon",
         dueDate: "2021-05-17",
         isDone: true
     }
 ];
 
-// Calls
-app.use(cors())
+//  Initialisation
+let MAX_ID=Math.max(...listTasks.map((task)=>task["id"]));
 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT_MINIPROJECT, ()=>{
+    console.log(`Application started and Listening on port ${PORT_MINIPROJECT}.`);
+});
+
+//  GET
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
-  });
+});
 
 app.get('/tasks', function(req, res){
     res.send(listTasks);
@@ -56,6 +71,20 @@ app.get('/tasks/undone', function(req, res){
     res.send(S);
 });
 
-app.listen(PORT_MINIPROJECT, ()=>{
-    console.log(`Application started and Listening on port ${PORT_MINIPROJECT}.`);
+//  POST
+app.post('/new_task', function (req, res) {
+    console.log(`Got body : ${req.body}`);
+    MAX_ID++;
+    let newTask=req.body;
+    newTask["id"]=MAX_ID;
+    listTasks.push(req.body);
+    
+    res.sendStatus(200);
+});
+
+// DELETE
+app.delete('/delete_task/:id', function (req, res) {
+    listTasks.delete();
+    
+    res.sendStatus(200);
 });
